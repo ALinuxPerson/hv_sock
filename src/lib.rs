128e4d871aab7f1f0887800ff17411ba8e8bfc30
ALinuxPerson <alinuxperson@gmail.com>
2024-08-15 18:31:51 +0800

    Add `host_registry` module

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,16 @@ mod listener;
 mod stream;
 mod addr;
 
+#[cfg(all(feature = "host-registry", windows))]
+pub mod host_registry;
+
 pub use socket::{Socket, TYPE, DOMAIN, PROTOCOL};
 pub use listener::{Listener, Incoming};
 pub use stream::Stream;
 pub use addr::SocketAddr;
+
+#[cfg(all(feature = "host-registry", windows))]
+pub use host_registry::{HostRegistry, Service, ServiceData, ServiceUuid};
 
 pub type HyperVSocket = Socket;
 pub type HyperVSocketListener = Listener;
@@ -16,3 +22,6 @@ pub type HyperVSocketAddr = SocketAddr;
 
 #[cfg(not(any(target_os = "linux", windows)))]
 compile_error!("hyper v sockets are only supported in linux and windows systems");
+
+#[cfg(all(not(windows), feature = "host-registry"))]
+compile_error!("the `host-registry` feature is only available on windows systems");
